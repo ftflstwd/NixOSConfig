e5df9165007b15ff9a0d49fa6c041c1af14e5976
ftflstwd <daniel@poston.info>
2025-07-24 10:34:26 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    {
+      nixosConfigurations = {
+        dsk = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ftflstwd = ./home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
